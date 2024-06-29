455d36287645f72f45811449b91dbe5b0444671e
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-06-29 20:32:21 -0300

    feat: add list foods route

--- routes/food.router.js
+++ routes/food.router.js
@@ -5,5 +5,6 @@ const validateFood = require("../middlewares/validateFood");
 const controller = require("../controller/food.controller");
 
 router.post("/foods", validateFood, controller.createFood);
+router.get("/foods", controller.listFoods);
 
 module.exports = router;
