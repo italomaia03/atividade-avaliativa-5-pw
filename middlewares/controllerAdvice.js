90e8b554d559852bca1b17bbaa6623f61bb6e1a0
Italo F O Maia <italo.fernandes.om@gmail.com>
2024-06-29 21:21:50 -0300

    fix: use if-else to avoid sending two responses

--- middlewares/controllerAdvice.js
+++ middlewares/controllerAdvice.js
@@ -5,17 +5,17 @@ class ControllerAdvice {
   execute(error, _req, res, _next) {
     if (error instanceof NotFoundError) {
       res.status(error.statusCode).json({
-        error: `${error.name}: ${error.message}`,
+        error: `${error.message}`,
       });
-    }
-
-    if (error instanceof BadRequestError) {
+    } else if (error instanceof BadRequestError) {
       res.status(error.statusCode).json({
-        error: `${error.name}: ${error.message}`,
+        error: `${error.message}`,
       });
+    } else if (error.name === "CastError") {
+      res.status(400).json({ error: `${error.message}` });
+    } else {
+      res.status(500).json({ error: error });
     }
-
-    res.status(500).json({ error: error });
   }
 }
 
